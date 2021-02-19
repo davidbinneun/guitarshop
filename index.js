8c2aee3385c8d61bdc941cf5fbbb345545f88d97
David Bin Neun <davidbinneun@gmail.com>
2021-02-19 11:14:54 +0200

    fixed indentation

--- index.js
+++ index.js
@@ -44,66 +44,66 @@ class ClassicGuitar {
 }
 
 class ElectricGuitar extends ClassicGuitar {
-constructor(manufactureYear, brand, price, longNeck) {
-    super(manufactureYear, brand, price);
-    this.longNeck = longNeck;
-}
+    constructor(manufactureYear, brand, price, longNeck) {
+        super(manufactureYear, brand, price);
+        this.longNeck = longNeck;
+    }
 
-play(){
-    this.price = this.price * 0.90;
-    this.used = true;
-    return "🎸🎸🎸";
-}
+    play(){
+        this.price = this.price * 0.90;
+        this.used = true;
+        return "🎸🎸🎸";
+    }
 
-static detectSound(sound){
-    switch(sound) {
-        case '🎶':
-            return 'ClassicGuitar';
-        case '🎸':
-            return 'ElectricGuitar';
-        case '🔊':
-            return 'BassGuitar';
-        default:
-            return 'dafuqIsThat';
-        }  
-}
+    static detectSound(sound){
+        switch(sound) {
+            case '🎶':
+                return 'ClassicGuitar';
+            case '🎸':
+                return 'ElectricGuitar';
+            case '🔊':
+                return 'BassGuitar';
+            default:
+                return 'dafuqIsThat';
+            }  
+    }
 }
 
 class BassGuitar extends ClassicGuitar {
-constructor(manufactureYear, brand, price) { 
-    super(manufactureYear, brand, price);
-    this.numberOfString = 4;
-}
-
-play(){
-    if (!this.used){
-        this.price = this.price * 0.90;
-        this.used = true;
+    constructor(manufactureYear, brand, price) { 
+        super(manufactureYear, brand, price);
+        this.numberOfString = 4;
     }
-    return "🔊🔊🔊";
-}
 
-playSolo(){
-    this.play();
-
-    let solo = '';
-    let characters = '💥🤘🎵📢💢🕺';
-    for ( let i = 0; i < 6; i++ ) {
-        solo += characters.charAt(Math.floor(Math.random() * 6));
+    play(){
+        if (!this.used){
+            this.price = this.price * 0.90;
+            this.used = true;
+        }
+        return "🔊🔊🔊";
     }
-    return solo;
-}
 
-static detectSound(sound){
-    switch(sound) {
-        case '🎶':
-            return 'ClassicGuitar';
-        case '🎸':
-            return 'ElectricGuitar';
-        case '🔊':
-            return 'BassGuitar';
-        default:
-            return 'dafuqIsThat';
-        }  
-}
+    playSolo(){
+        this.play();
+
+        let solo = '';
+        let characters = '💥🤘🎵📢💢🕺';
+        for ( let i = 0; i < 6; i++ ) {
+            solo += characters.charAt(Math.floor(Math.random() * 6));
+        }
+        return solo;
+    }
+
+    static detectSound(sound){
+        switch(sound) {
+            case '🎶':
+                return 'ClassicGuitar';
+            case '🎸':
+                return 'ElectricGuitar';
+            case '🔊':
+                return 'BassGuitar';
+            default:
+                return 'dafuqIsThat';
+            }  
+    }
 }
